3294857a51345dda3466d756307b671ec576d166
appiumguide <appiumguide.com>
2025-01-24 00:17:55 +0530

    Added new package for gestures plugin

--- src/main/java/com/appiumguide/demo1/Sample.java
+++ src/main/java/com/appiumguide/demo1/Sample.java
@@ -1,12 +1,15 @@
 
 package com.appiumguide.demo1;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import com.google.common.collect.ImmutableMap;
 
+import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import java.net.*;
@@ -31,7 +34,6 @@ public class Sample {
 	
 	@Test
 	public void test() throws InterruptedException {
-		
 		
 		
 	}
