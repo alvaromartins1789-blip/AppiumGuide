26db84797546de887b6e8c2b4c509db848d20bcd
appiumguide <appiumguide.com>
2025-07-18 12:49:29 +0530

    Added AppiumOptions_Demo Script & appium-config with appiumoptions.json file

--- src/test/java/com/appiumguide/capabilities/AppiumOptions_Demo.java
+++ src/test/java/com/appiumguide/capabilities/AppiumOptions_Demo.java
@@ -1,5 +1,4 @@
-
-package demo;
+package com.appiumguide.capabilities;
 
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -14,35 +13,45 @@ import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Sample {
+/**
+ * Script Details . ‘Appium’ – “Capabilities”  -  Complete Details on “appium:options”
+ * 
+ * appium-java-client version: Latest
+ * 
+ * @author 'Ramesh Kodumuru' for AppiumGuide [appiumguide@gmail.com]
+ */
+
+public class AppiumOptions_Demo {
 	
 	private AndroidDriver driver;
-	
+	File screenshot;
 								
 	@BeforeTest
 	public void setup() throws MalformedURLException {
 		
-		 
-        String basepath=System.getProperty("user.dir")+"/APK_Files/";
+		//Install apk by using desired capability
+		String basepath=System.getProperty("user.dir")+"/APK_Files/";
 		String apkpath=basepath+"ApiDemos.apk";
 		
-		Map<String,Object> appiumoptions=new HashMap<>();
 		
-		appiumoptions.put("platformVersion", "13");
-		appiumoptions.put("automationName", "uiautomator2");
-        appiumoptions.put("deviceName", "Pixel6");
-       	appiumoptions.put("app", apkpath);
-        appiumoptions.put("noReset", true);
-        appiumoptions.put("autoGrantPermissions", true);
-        appiumoptions.put("newCommandTimeout", 120);
+		//appium:options container
+		Map<String,Object> appiumOptions=new HashMap<>();
+		
+		appiumOptions.put("platformVersion", "13");
+		appiumOptions.put("automationName", "uiautomator2");
+		appiumOptions.put("deviceName", "Pixel6");
+		appiumOptions.put("app", apkpath);
+		appiumOptions.put("noReset", true);
+		appiumOptions.put("autoGrantPermissions", true);
+		appiumOptions.put("newCommandTimeout", 120);
 		
 		UiAutomator2Options cap=new UiAutomator2Options();
+		//PlatformName at root-level
 		cap.setPlatformName("android");
-		cap.setCapability("appium:options", appiumoptions);
+		
+		cap.setCapability("appium:options", appiumOptions);	
 		
 		
-		
-					
 		driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), cap);
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(100));
 		
@@ -51,7 +60,7 @@ public class Sample {
 	@Test
 	public void test() throws InterruptedException, IOException {
 		
-		System.out.println(driver.getSessionId());
+		
 		        
 	}
 	
@@ -61,7 +70,7 @@ public class Sample {
 	public void teardown() {
 		
 		if(driver!=null) {
-			//driver.quit();
+			driver.quit();
 			System.out.println("Test Completed");
 		}
 		
@@ -69,4 +78,5 @@ public class Sample {
 	
 
 }
+
 
