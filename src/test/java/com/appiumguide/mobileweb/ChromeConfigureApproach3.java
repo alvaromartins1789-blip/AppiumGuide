4970d93bbcd2350e92722f5ad9874e57448b15e0
appiumguide <appiumguide.com>
2025-08-15 11:01:32 +0530

    Added 'ChromeConfigureApproach3' Script

---
+++ src/test/java/com/appiumguide/mobileweb/ChromeConfigureApproach3.java
@@ -0,0 +1,63 @@
+package com.appiumguide.mobileweb;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+import java.io.IOException;
+import java.net.*;
+import java.time.Duration;
+import java.net.URL;
+
+/**
+ * Script Details - 5 Ways to Configure ‘ChromeDriver’ in Appium --> Approach-3 : By Auto-Install via Appium 2.x(npm)
+ * 
+ * appium-java-client version: 9.5.0
+ * 
+ * @author 'Ramesh Kodumuru' for AppiumGuide [appiumguide@gmail.com]
+ */
+
+public class ChromeConfigureApproach3 {
+	
+	private AndroidDriver driver;
+					
+	@BeforeTest
+	public void setup() throws MalformedURLException {
+		UiAutomator2Options cap=new UiAutomator2Options();
+		cap.setPlatformName("android");
+		cap.setAutomationName("uiautomator2");
+		cap.setCapability("browser","chrome");
+		
+		//Note: No need to provide chromedriver.exe path
+							
+		driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), cap);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(100));
+		
+	}
+	
+		
+	@Test
+	public void test() throws InterruptedException, IOException {
+		
+			driver.get("https://appiumguide-by-kodumururamesh.blogspot.com/p/home-page.html");        
+		   
+	}
+				
+		
+	@AfterTest
+	public void teardown() {
+		
+		if(driver!=null) {
+			driver.quit();
+			System.out.println("Test Completed");
+		}
+		
+	}
+	
+
+}
+
+
